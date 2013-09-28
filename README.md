3801b4b2a3eee5ab7323f031c990730eef467e48
francois <francoisrvespa@gmail.com>
2013-09-28 12:00:54 +0200

    0.0.2

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 apt
 ======
 
-## Very, very Alpha!
-
 `apt-get` meets `npm`! For every dependencies of a project `npm` does not take care of (`mongodb`, `curl`, etc.) -- and that you would have usually to install globally on a machine (provided you are sudo!). `apt` builds the sources in a given folder inside your project, so you don't need to be root. The file `local.json` behaves as a `package.json` with npm - you can install dependencies declared in this file by running `apt install`. `apt` can be invoked inside a node script through `require` or be invoked directly from the terminal if you installed it globally.
+
+# Install
+
+	npm install -g git+https://github.com/co2-git/apt.git
 
 # Features
 
--- bin/apt.js
+++ bin/apt.js
@@ -2,20 +2,53 @@ for ( var args = [], i = 2; i < process.argv.length; i++ ) {
   args.push(process.argv[i]);
 }
 
-var action = args[0],
-  Local = require('../local.json'),
-  Store = require('../store.json');
+var action = args[0];
+
+require('colors');
+
+var onMessage = require('../lib/helpers/onMessage');
 
 if ( ! action ) {
-  console.log({local: Local.length, store: Store.length});
+  var package = require('../package.json');
+  console.log('apt'.bold.white + (' version ' + package.version).grey);
+  console.log(package.description);
+  console.log(('Type ' + 'apt help'.italic + ' for help').grey);
+  console.log();
+  var AptJson = new (require('../lib/AptJson'))(process.cwd());
+  if ( ! AptJson.FileExists || ! AptJson.Size ) {
+    console.log('No modules installed'.red);
+  }
+  var available = require('../Store.json').length;
+  console.log((available + ' modules available in Store').green);
+  console.log();
+  console.log('Thank you for using apt'.italic + ' More info at github.com/co2/apt'.italic.grey);
 } else {
   args.shift();
   switch ( action ) {
     case 'view':
-      console.log(JSON.stringify(Local, null, 2));
+      var Local = new (require('../lib/Local'))();
+      Local.get({}, function (error, modules) {
+        if ( error ) {
+          if ( error instanceof Error ) {
+            switch ( error.toString() ) {
+              case 'Error: apt.json not found':
+                return console.log('apt.json not found -- nothing installed so far here!');
+              default:
+                throw error;
+            }
+          }
+          throw error;
+        }
+        if ( ! modules.length ) {
+          return console.log('Nothing installed so far here!'.blue);
+        }
+        console.log(JSON.stringify(modules, null, 2));
+      });
       break;
     case 'search':
-      var found = [], where = {};
+      var found = [],
+        where = {},
+        search = require('../lib/search');
       args.forEach(function (arg) {
         if ( arg.match(/=/) ) {
           where[arg.split('=')[0]] = arg.split('=')[1];
@@ -23,48 +56,30 @@ if ( ! action ) {
           where.name = arg;
         }
       });
-      Store.forEach(function (module) {
-        var match = true;
-        if ( Object.keys(where).length ) {
-          for ( var key in where ) {
-            if ( module[key] !== where[key] ) {
-              match = false;
-            }
-          }
-        }
-        if ( match ) {
-          found.push({name: module.name, type: module.type, latest: module.latest.static});
-        }
-      });
-      console.log(JSON.stringify(found, null, 2));
-      break;
-    case 'install':
-      if ( ! args.length ) {
-        Local.forEach(function (module) {
-          console.log('Installing ' + module.name + ' version ' + module.version);
-          require('../lib/install')([module.name, module.version].join('@'), function (error, module) {
-            if ( error ) {
-              if ( typeof error === 'string' ) {
-                switch ( error ) {
-                  case 'Already installed':
-                    console.log(module.name + ' version ' + module.version + ' is already installed');
-                    break;
-                  default:
-                    throw new Error(error);
-                }
+      search(where)
+        .on('error', function (error) {
+          throw error;
+        })
+        .on('done', function (storedModules) {
+            storedModules.forEach(function (module) {
+              for ( var tab1 = '', i = module.name.length; i < 10; i ++ ) {
+                tab1 += ' ';
               }
-              else {
-                throw error;
+              
+              var dependencies = ( module.dependencies ? Object.keys(module.dependencies).length +
+                ' dependenc' + ( Object.keys(module.dependencies).length === 1 ? 'y' : 'ies' ) : 'no dependency' );
+              
+              for ( var tab2 = '', j = module.type.length; j < 20; j ++ ) {
+                tab2 += ' ';
               }
-            }
+              
+              console.log(('▹ ' + module.name).green + tab1 + module.type + tab2 + dependencies.grey);
           });
         });
-      }
-      else {
-        var module = args[0].split('@')[0],
-          version = args[0].split('@')[1];
-        console.log('Installing ' + module + ' version ' + version);
-        require('../lib/install')([module, version].join('@'), function (error, module) {
+      break;
+    case 'install':
+      var install = require('../lib/install'),
+        installer = function (error, module) {
           if ( error ) {
             if ( typeof error === 'string' ) {
               switch ( error ) {
@@ -79,7 +94,52 @@ if ( ! action ) {
               throw error;
             }
           }
+          console.log('Installed'.green, module);
+        };
+      if ( ! args.length ) {
+        Local.forEach(function (module) {
+          console.log('Installing ' + module.name + ' version ' + module.version);
+          require('../lib/install')([module.name, module.version].join('@'), function (error, module, message) {
+            console.log(args);
+            if ( error ) {
+              if ( typeof error === 'string' ) {
+                switch ( error ) {
+                  case 'Already installed':
+                    console.log(module.name + ' version ' + module.version + ' is already installed');
+                    break;
+                  default:
+                    throw new Error(error);
+                }
+              }
+              else {
+                throw error;
+              }
+            }
+            else if ( ! module && message ) {
+              console.log(message);
+              switch ( Object.keys(message)[0] ) {
+                default:
+                  var x = ['Installing'.blue,
+                    message[Object.keys(message)[0]].grey.italic];
+                  if ( message.debug ) {
+                    x.push(message.debug);
+                  }
+                  console.error.apply(null, x);
+                  return;
+              }
+            }
+          });
         });
+      }
+      else {
+        install(args[0])
+          .set('message prefix', ['Installing '.blue + args[0].bold.blue])
+          .on('error', function (error) {
+            throw error;
+          })
+          .on('done', function (module) {
+            console.log(module);
+          });
       }
       break;
     case 'fetch':
